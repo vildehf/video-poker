c877fc786ffd2dc2725b53322d4d08d7a9be422e
vildehf <vildefristad@gmail.com>
2026-09-07 13:22:23 +0200

    fix: typo

--- src/pages/RulesPage.tsx
+++ src/pages/RulesPage.tsx
@@ -2,7 +2,7 @@ export default function RulesPages() {
   return (
     <main>
       <h1>Regler</h1>
-      <p>Her kmmer regler og utbetalinger.</p>
+      <p>Her kommer regler og utbetalinger.</p>
     </main>
   );
 }
