20bb38a4bc5bfbc6b46b410c6d6961f75950d6e4
vildehf <vildefristad@gmail.com>
2026-09-07 13:58:25 +0200

    feat: display poker hand

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,16 +1,25 @@
 import Card from "../components/Card/Card";
 import type { PlayingCard } from "../types/PlayingCard";
+import styles from "./GamePage.module.css";
 
 export default function GamePage() {
-  const testCard: PlayingCard = {
-    suit: "hearts",
-    value: "A",
-  };
+  const testCards: PlayingCard[] = [
+    { suit: "hearts", value: "A" },
+    { suit: "diamonds", value: "K" },
+    { suit: "clubs", value: "Q" },
+    { suit: "spades", value: "J" },
+    { suit: "hearts", value: "10" },
+  ];
 
   return (
     <main>
       <h1>Video Poker</h1>
-      <Card card={testCard} />
+
+      <div className={styles.hand}>
+        {testCards.map((card) => (
+          <Card key={`${card.suit}-${card.value}`} card={card} />
+        ))}
+      </div>
     </main>
   );
 }
