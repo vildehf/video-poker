127f68c93c3b75f1e35af0efc988214208185ef0
vildehf <vildefristad@gmail.com>
2026-09-09 17:55:22 +0200

    feat: add deck and card dealing

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import type { PlayingCard } from "../types/PlayingCard";
 import Card from "../components/Card/Card";
 import createDeck from "../utils/createDeck";
 import styles from "./GamePage.module.css";
@@ -7,15 +9,21 @@ export default function GamePage() {
   const deck = createDeck();
   const shuffledDeck = shuffleDeck(deck);
 
+  const [hand, setHand] = useState<PlayingCard[]>(shuffledDeck.slice(0, 5));
+  function dealNewHand() {
+    const newDeck = shuffleDeck(createDeck());
+    setHand(newDeck.slice(0, 5));
+  }
+
   return (
     <main>
       <h1>Video Poker</h1>
-
       <div className={styles.hand}>
-        {shuffledDeck.slice(0, 5).map((card) => (
+        {hand.map((card) => (
           <Card key={`${card.suit}-${card.value}`} card={card} />
         ))}
       </div>
+      <button onClick={dealNewHand}>Del ut nye kort</button>
     </main>
   );
 }
