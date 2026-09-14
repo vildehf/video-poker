f52751b441ea473db0e1408cd966dc1590bfe587
vildehf <vildefristad@gmail.com>
2026-09-14 13:51:35 +0200

    feat: persist game hand with Zustand

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 import CurrentBet from "../CurrentBet/CurrentBet";
 import TotalCoins from "../TotalCoins/TotalCoins";
-import type { PlayingCard } from "../../types/PlayingCard";
 import type { PokerHand } from "../../types/PokerHand";
 import Card from "../Card/Card";
 import createDeck from "../../utils/createDeck";
 import shuffleDeck from "../../utils/shuffleDeck";
 import styles from "../../pages/GamePage/GamePage.module.css";
 import { useGameStore } from "../../store/useGameStore";
+import checkPokerHand from "../../utils/checkPokerHand";
 
 export default function Game() {
   const deck = createDeck();
   const shuffledDeck = shuffleDeck(deck);
-  const [hand, setHand] = useState<PlayingCard[]>(shuffledDeck.slice(0, 5));
+  const hand = useGameStore((state) => state.hand);
+  const setHand = useGameStore((state) => state.setHand);
+
+  if (hand.length === 0) {
+    setHand(shuffledDeck.slice(0, 5));
+  }
   const [currentBet, setCurrentBet] = useState(1);
   const [PokerHand, setPokerHand] = useState<PokerHand>("Høyt kort");
   const [heldCards, setHeldCards] = useState<number[]>([]);
@@ -32,39 +37,6 @@ export default function Game() {
     } else {
       setHeldCards([...heldCards, index]);
     }
-  }
-
-  function hasPair(cards: PlayingCard[]) {
-    return cards.some((card, index) =>
-      cards.some(
-        (otherCard, otherIndex) =>
-          index !== otherIndex && card.value === otherCard.value,
-      ),
-    );
-  }
-
-  function hasTwoPairs(cards: PlayingCard[]) {
-    const pairValues: string[] = [];
-
-    cards.forEach((card) => {
-      const matchingCards = cards.filter(
-        (otherCard) => otherCard.value === card.value,
-      );
-
-      if (matchingCards.length === 2 && !pairValues.includes(card.value)) {
-        pairValues.push(card.value);
-      }
-    });
-    return pairValues.length === 2;
-  }
-
-  function hasThreeOfAKind(cards: PlayingCard[]) {
-    return cards.some((card) => {
-      const matchingCards = cards.filter(
-        (otherCard) => otherCard.value === card.value,
-      );
-      return matchingCards.length === 3;
-    });
   }
 
   function dealNewHand() {
@@ -95,15 +67,7 @@ export default function Game() {
     setHand(newHand);
     setHeldCards([]);
 
-    if (hasThreeOfAKind(newHand)) {
-      setPokerHand("Tre like");
-    } else if (hasTwoPairs(newHand)) {
-      setPokerHand("To par");
-    } else if (hasPair(newHand)) {
-      setPokerHand("Par");
-    } else {
-      setPokerHand("Høyt kort");
-    }
+    setPokerHand(checkPokerHand(newHand));
   }
 
   return (
--- src/store/useGameStore.ts
+++ src/store/useGameStore.ts
@@ -1,10 +1,14 @@
 import { create } from "zustand";
 import type { Player } from "../types/Player";
 import { persist } from "zustand/middleware";
+import type { PlayingCard } from "../types/PlayingCard";
 
 type GameStore = {
   currentPlayer: Player | null;
   setCurrentPlayer: (player: Player) => void;
+
+  hand: PlayingCard[];
+  setHand: (hand: PlayingCard[]) => void;
 };
 
 /**
@@ -14,9 +18,14 @@ export const useGameStore = create<GameStore>()(
   persist(
     (set) => ({
       currentPlayer: null,
+      hand: [],
 
       setCurrentPlayer: (player) => {
         set({ currentPlayer: player });
+      },
+
+      setHand: (hand) => {
+        set({ hand });
       },
     }),
     {
